ebdc0eb6d536e30a43cad943d91dad25bb6bd312
koiitirou <koiitirou@gmail.com>
2025-02-20 23:18:30 +0900

    u

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["*.js", "*.jsx", "*.mjs", "*.cjs"], // TypeScript を使用しないファイルを指定
+    languageOptions: {
+      parserOptions: {
+        project: null, // TypeScript の設定を無効化
+      },
+    },
+  },
+];
 export default eslintConfig;
