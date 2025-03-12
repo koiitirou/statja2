abd9ad3c009a6e826e016e07b733297d0dbcf451
koiitirou <koiitirou@gmail.com>
2025-03-12 22:31:21 +0900

    prescription

---
+++ app/ndb/prescription/[id2]/content.js
@@ -0,0 +1,42 @@
+"use client";
+
+import HideBar from "components/layout/hidebar";
+// import NextBreadcrumbs from "components/function/bcv15";
+// import { Typography, Box } from "@mui/material";
+// import Windowed from "components/function/prescription_windowed";
+// import Pyramid2 from "components/function/Prescription_pyramid";
+
+// const rep1 = {
+//   ndb: "処方薬ランキング",
+//   prescription: "処方薬",
+// };
+
+const Content = () =>
+  // { title, description, ssg1 }
+
+  {
+    // const con_name = ssg1.def.dn2;
+    return (
+      <HideBar>
+        aa
+        {/* <NextBreadcrumbs rep1={rep1} />
+      <Windowed />
+      <Typography variant="h1">{title}</Typography>
+      <Typography variant="body1">
+        {"　"}
+        {description}
+      </Typography>
+      <Typography variant="body1">
+        {"　"}
+        {con_name}の売上は{ssg1.dat[0].val.sl.toLocaleString()}
+        円、薬価は
+        {ssg1.def.ei2 == "ind" && <>{ssg1.def.dp2}、</>}処方数は
+        {ssg1.dat[0].val.pf}
+        {ssg1.def.unt}でした。
+      </Typography>
+      <Pyramid2 res2={ssg1} con_name={con_name} /> */}
+      </HideBar>
+    );
+  };
+
+export default Content;
---
+++ app/ndb/prescription/[id2]/page.js
@@ -0,0 +1,54 @@
+import Content from "./content";
+import pre_path0 from "components/data/path_ndb/sum_prescription_path.json";
+// import { server } from "components/data/config";
+
+export const dynamicParams = false;
+
+const pre_path = pre_path0.path;
+
+export async function generateStaticParams() {
+  const res3 = pre_path.map((v) => v.params);
+  return res3;
+}
+
+// function generateTitleAndDescription(ssg1) {
+//   const con_name = ssg1.def.dn2;
+//   const d0 = ssg1.dat[0];
+//   const l0 = ssg1.dat[ssg1.dat.length - 1];
+//   const title = `${con_name}の売上・処方数・薬価${l0.yrs}~${d0.yrs}【${ssg1.def.kbd}】`;
+
+//   const description = `${con_name}の${l0.yrs}~${d0.yrs}年の売上、薬価、${ssg1.def.shn}処方数（年齢(5歳階級)、性別、都道府県別）をまとめました。`;
+
+//   return { title, description };
+// }
+
+// async function fetchAndProcessData(id2) {
+//   const res = await fetch(`${server}/prescription/${id2}.json`);
+//   const ssg1 = await res.json();
+//   return ssg1;
+// }
+
+// export async function generateMetadata({ params }) {
+//   const { id2 } = await params;
+//   const ssg1 = await fetchAndProcessData(id2);
+//   const { title, description } = generateTitleAndDescription(ssg1);
+//   return {
+//     title: title,
+//     description: description,
+//     openGraph: {
+//       title: title,
+//       description: description,
+//     },
+//   };
+// }
+
+export default async function Page({ params }) {
+  // const { id2 } = await params;
+  // const ssg1 = await fetchAndProcessData(id2);
+  // const { title, description } = generateTitleAndDescription(ssg1);
+  return (
+    <>
+      <Content />
+    </>
+  );
+}
